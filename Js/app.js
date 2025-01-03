48e97ad6430b36146c7ae1d7fcc40816413c19d5
Valentin Baurens-Torrente <valentin200625@outlook.fr>
2025-01-03 15:36:36 +0100

    added light mode and other easter egg

--- Js/app.js
+++ Js/app.js
@@ -2,27 +2,67 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.getElementById('menuToggle');
+    const menuToggleDark = document.getElementById('menuToggleDark');
+    const menuToggleLight = document.getElementById('menuToggleLight');
     const navMenu = document.querySelector('.nav-menu');
     const mainContent = document.querySelector('main');
     const body = document.body;
 
-    // Ouvrir/Fermer 
-    menuToggle.addEventListener('click', () => {
-        const isOpen = navMenu.classList.contains('nav-open');
-        
-        if (isOpen) {
-            navMenu.classList.remove('nav-open');
-        } else {
-            navMenu.classList.add('nav-open');
-        }
-    });
+    const toggleMenu = (menuToggle) => {
+        menuToggle.addEventListener('click', () => {
+            const isOpen = navMenu.classList.contains('nav-open');
+            
+            if (isOpen) {
+                navMenu.classList.remove('nav-open');
+            } else {
+                navMenu.classList.add('nav-open');
+            }
+        });
+    };
+
+    toggleMenu(menuToggleDark);
+    toggleMenu(menuToggleLight);
 
     // Fermer la navbar en cliquant en dehors
     body.addEventListener('click', (dehors) => {
-        if (!navMenu.contains(dehors.target) && !menuToggle.contains(dehors.target)) {
+        if (!navMenu.contains(dehors.target) && !menuToggleDark.contains(dehors.target) && !menuToggleLight.contains(dehors.target)) {
             navMenu.classList.remove('nav-open');
             mainContent.classList.remove('main-shift');
         }
     });
+});
+
+//permet de changer de stylesheet
+const changeStylesheet = () => {
+    const stylesheet = document.getElementById('stylesheet');
+    const favicon = document.getElementById('favicon');
+
+    if (stylesheet.getAttribute('href') === '/Styles/styles.css') {
+        stylesheet.setAttribute('href', '/Styles/alternate-styles.css');
+        favicon.setAttribute('href', 'Img/light-logo.ico');
+        localStorage.setItem('stylesheet', '/Styles/alternate-styles.css');
+        localStorage.setItem('favicon', 'Img/light-logo.ico');
+    } else {
+        stylesheet.setAttribute('href', '/Styles/styles.css');
+        favicon.setAttribute('href', 'Img/dark-logo.ico');
+        localStorage.setItem('stylesheet', '/Styles/styles.css');
+        localStorage.setItem('favicon', 'Img/dark-logo.ico');
+    }
+};
+
+const loadSavedStylesheet = () => {
+    const savedStylesheet = localStorage.getItem('stylesheet');
+    const savedFavicon = localStorage.getItem('favicon');
+    if (savedStylesheet) {
+        document.getElementById('stylesheet').setAttribute('href', savedStylesheet);
+    }
+    if (savedFavicon) {
+        document.getElementById('favicon').setAttribute('href', savedFavicon);
+    }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const changeStylesheetButton = document.getElementById('changeStylesheet');
+    changeStylesheetButton.addEventListener('click', changeStylesheet);
+    loadSavedStylesheet();
 });
